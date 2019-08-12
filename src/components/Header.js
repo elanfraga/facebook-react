0456d975ca486b8d932bb225529f18e76548515a
Elan Fraga <elanfraga@gmail.com>
2019-08-11 23:48:57 -0300

    estilizando header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { FaUserAlt } from "react-icons/fa";
 
 function Header() {
-  return <header>Headers</header>;
+  return (
+    <header className="content">
+      <nav className="content">
+        <h1>facebook</h1>
+        <div>
+          <span>Meu perfil</span>
+          <FaUserAlt color="white" size={15} className="acount" />
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
